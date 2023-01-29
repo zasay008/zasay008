74c973e0cadc1765d11fa4b0214c73716ce2842c
zasay008 <104813341+zasay008@users.noreply.github.com>
2023-01-30 01:47:46 +0700

    Add files via upload

---
+++ asspos/dataclass/ProductModel.kt
@@ -0,0 +1,12 @@
+package com.example.asspos.dataclass
+
+data class ProductModel (
+
+    private var product_barcode: String? = null,
+    private var product_name: String? = null,
+    private var product_cat: String? = null,
+    private var product_stk: String? = null,
+    private var product_pri: String? = null
+
+
+)
