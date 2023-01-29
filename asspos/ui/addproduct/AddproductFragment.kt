232f09d2e8f6706c03ff0d9427b13d829c6959fe
zasay008 <104813341+zasay008@users.noreply.github.com>
2023-01-30 01:49:00 +0700

    Add files via upload

--- asspos/ui/addproduct/AddproductFragment.kt
+++ asspos/ui/addproduct/AddproductFragment.kt
@@ -1,21 +1,90 @@
 package com.example.asspos.ui.addproduct
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.example.asspos.Manifest
 import com.example.asspos.R
+import com.example.asspos.databinding.FragmentAddproductBinding
+import com.example.asspos.dataclass.ProductModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.net.URI
 
 class AddproductFragment : Fragment() {
+
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var bar_pro: EditText
+    private lateinit var name_pro: EditText
+    private lateinit var cat_pro: EditText
+    private lateinit var stk_pro: EditText
+    private lateinit var pri_pro: EditText
+    private lateinit var add_pro: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_addproduct, container, false)
+    ): View {
 
-        return view;
+        val v: View = inflater.inflate(R.layout.fragment_addproduct, container, false)
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        bar_pro = v.findViewById(R.id.barcode)
+        name_pro = v.findViewById(R.id.namepro)
+        cat_pro = v.findViewById(R.id.category_pro)
+        stk_pro = v.findViewById(R.id.stock_pro)
+        pri_pro = v.findViewById(R.id.price_pro)
+        add_pro = v.findViewById(R.id.addbtn)
+
+        add_pro.setOnClickListener {
+
+            addproduct()
+
+        }
+
+        return v
+    }
+
+    private fun addproduct() {
+
+        val bar_txt = bar_pro.text.toString()
+        val name_txt = name_pro.text.toString()
+        val cat_txt = cat_pro.text.toString()
+        val stk_txt = stk_pro.text.toString()
+        val pri_txt = pri_pro.text.toString()
+        var users: FirebaseUser? = auth.currentUser
+        val finaluser = users?.email
+        var resultemmail = finaluser?.replace(".","")
+        if (bar_txt.isEmpty() || name_txt.isEmpty() || cat_txt.isEmpty() || stk_txt.isEmpty() || pri_txt.isEmpty()){
+            val productModel: ProductModel = ProductModel(bar_txt, name_txt, cat_txt, stk_txt, pri_txt)
+            if (resultemmail != null) {
+                databaseReference.child(resultemmail).child("ProductModel").child(bar_txt).setValue(productModel)
+                bar_pro.text
+                name_pro.text
+                cat_pro.text
+                stk_pro.text
+                pri_pro.text
+                Toast.makeText(activity, name_txt + "ถูกเพิ่มเข้าไปสำเร็จ", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(activity, "กรุณาตรวจสอบความถูกต้องอีกครั้ง", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
